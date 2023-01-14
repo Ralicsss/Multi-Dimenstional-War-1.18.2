1a83ccae532ef552f0eedcc5a2ca5a3e1880c0fb
2266Flash <49455863+2266Flash@users.noreply.github.com>
2023-01-14 12:38:44 +0000

    Delayed

--- src/main/java/me/ralics/multidimensionalwar/Events/Onjoin.java
+++ src/main/java/me/ralics/multidimensionalwar/Events/Onjoin.java
@@ -1,5 +1,6 @@
 package me.ralics.multidimensionalwar.Events;
 
+import me.ralics.multidimensionalwar.Scoreboard.EventManager;
 import me.ralics.multidimensionalwar.Scoreboard.ScoreBoardManager;
 import me.ralics.multidimensionalwar.Scoreboard.TeamManager;
 import org.bukkit.Bukkit;
@@ -18,21 +19,31 @@ public class Onjoin {
                 p.sendMessage("You are on a team");
             } else if (!t.hasEntry(p.getName())) {
                 p.sendMessage("you are not on a team");
-                Bukkit.getScheduler().scheduleSyncDelayedTask()
-                p.setGameMode(GameMode.SURVIVAL);
-                if(Bukkit.getOnlinePlayers().size() % 3 == 0){
-                    TeamManager.OverWorld.addEntry(p.getName());
-                    Location loc = new Location(Bukkit.getWorld("world"), 90, 64, 80, 0, 0);
-                    p.teleport(loc);
-                } else if (Bukkit.getOnlinePlayers().size()%3 ==1){
-                    TeamManager.Nether.addEntry(p.getName());
-                    Location loca = new Location(Bukkit.getWorld("world_nether"), 185, 96, 166, 0, 0);
-                    p.teleport(loca);
-                } else if (Bukkit.getOnlinePlayers().size()%3 == 2){
-                    TeamManager.End.addEntry(p.getName());
-                    Location locat = new Location(Bukkit.getWorld("world_the_end"), -2975, 58, -1160, 0, 0);
-                    p.teleport(locat);
-                }
+                Bukkit.getScheduler().scheduleSyncDelayedTask(EventManager.plugin, new Runnable() {
+                            @Override
+                            public void run() {
+                                if(Bukkit.getOnlinePlayers().size() % 3 == 0){
+                                    TeamManager.OverWorld.addEntry(p.getName());
+                                    Location loc = new Location(Bukkit.getWorld("world"), 90, 64, 80, 0, 0);
+                                    p.teleport(loc);
+                                } else if (Bukkit.getOnlinePlayers().size()%3 ==1){
+                                    TeamManager.Nether.addEntry(p.getName());
+                                    Location loca = new Location(Bukkit.getWorld("world_nether"), 185, 96, 166, 0, 0);
+                                    p.teleport(loca);
+                                } else if (Bukkit.getOnlinePlayers().size()%3 == 2){
+                                    TeamManager.End.addEntry(p.getName());
+                                    Location locat = new Location(Bukkit.getWorld("world_the_end"), -2975, 58, -1160, 0, 0);
+                                    p.teleport(locat);
+                                }
+                            }
+                        },20L);
+                Bukkit.getScheduler().scheduleSyncDelayedTask(EventManager.plugin, new Runnable() {
+                    @Override
+                    public void run() {
+                        p.setGameMode(GameMode.SURVIVAL);
+                    }
+                },40L);
+                        
             }
         }
 
