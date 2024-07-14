c40960f81cc5d0fb83f21442f4570e5b2339cad7
Ralics <103959154+Ralicsss@users.noreply.github.com>
2024-07-14 20:43:08 +0200

    Update Punishments.java

--- src/main/java/me/ralics/multidimensionalwar/Commands/Punishments.java
+++ src/main/java/me/ralics/multidimensionalwar/Commands/Punishments.java
@@ -1,10 +1,10 @@
 public class Punishments{
 
     public static Punishment HACKUSATING;
-    public static Punishment HACKUSATING;
-    public static Punishment HACKUSATING;
-    public static Punishment HACKUSATING;
-    public static Punishment HACKUSATING;
+    public static Punishment XRAY;
+    public static Punishment SPAM;
+    public static Punishment SWEARING;
+    public static Punishment HACKED_CLIENT;
     public static Punishment HACKUSATING;
     public static Punishment HACKUSATING;
 
@@ -12,6 +12,19 @@ public class Punishments{
 
     private Punishments(){
         INSTANCE = this;
+
+        Strike xrayStrike = new Strike(Strikes.BAN);
+        xrayStrike.addPunishment(1, 15);
+        xrayStrike.addPunishment(2, 30);
+        xrayStrike.addPunishment(3, -1); //Permanent ban
+
+        XRAY = new Punishment(xrayStrike);
+        
+        Strike spamStrike = new Strike(Strikes.MUTE);
+        spamStrike.addPunishment(1, 10);
+        spamStrike.addPunishment(2, 30);
+        spamStrike.addPunishment(3, 60);
+        spamStrike.addPunishment(4, -1);
     }
 
     public static Punishments getInstance(){
