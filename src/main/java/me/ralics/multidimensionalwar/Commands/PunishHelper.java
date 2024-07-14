0ed6d11cd16045cfd3fc7b68923b8e1c4edf229b
Ralics <103959154+Ralicsss@users.noreply.github.com>
2024-07-14 20:28:08 +0200

    Update PunishHelper.java

--- src/main/java/me/ralics/multidimensionalwar/Commands/PunishHelper.java
+++ src/main/java/me/ralics/multidimensionalwar/Commands/PunishHelper.java
@@ -15,8 +15,8 @@ public class PunishHelper{
         ItemStack griefing = createItem(basicColor + "Griefing", mat, 8);
         ItemStack swearing = createItem(basicColor + "Swearing", mat, 9);
         ItemStack hackusating = createItem(basicColor + "Hackusating", mat, 10);
-        punishments.put(hackusating, Punishment.HACKUSATING);
-        punishments.put(hackedClient, Punishment.HACKED_CLIENT);
+        punishments.put(hackusating, Punishments.HACKUSATING);
+        punishments.put(hackedClient, Punishment.sHACKED_CLIENT);
         punishments.put(xray, Punishments.XRAY)
         punishments.put(spamming, Punishments.SPAM);
         punishments.put(swearing, Punishments.SWEARING);
