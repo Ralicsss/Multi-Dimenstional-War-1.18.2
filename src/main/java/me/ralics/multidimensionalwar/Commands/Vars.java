5a817351be6ac75eee754d90aee5096aac1d5c1c
Ralics <103959154+Ralicsss@users.noreply.github.com>
2024-07-14 20:26:52 +0200

    Update Vars.java

---
+++ src/main/java/me/ralics/multidimensionalwar/Commands/Vars.java
@@ -0,0 +1,6 @@
+public class Vars{
+    public static PunishPlugin PLUGIN;
+    public static ChatColor SUCCESS_COLOR = ChatColor.GREEN;
+    public static ChatColor ERROR_COLOR = ChatColor.RED;
+    public static ChatColor SECONDARY_COLOR = ChatColor.CYAN;
+}
